8de1bb2117480e7367b2d56f3a478d5a38784135
William Kimball <wkkimball043@gmail.com>
2018-06-04 09:38:46 -0500

    added comments

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+//Change the first article header to the read "Welcome William's blog"
 document.querySelector(".article__header").textContent = "Welcome William's blog";
 
+//select all article headers
 const classes = document.querySelectorAll(".article__header");
 
 console.log(classes)
 
+//add important to all article headers
 for (i = 0; i < classes.length; i++) {
     classes[i].classList = "article__header important"
 }
